fbda8d874e5a76a7b3dda952009138a073d44b06
Trey Evans <lewis.r.evans@gmail.com>
2024-09-18 13:57:44 -0400

    Fix http subscriber routing.

--- lib/event_source/protocols/http/faraday_queue_proxy.rb
+++ lib/event_source/protocols/http/faraday_queue_proxy.rb
@@ -49,10 +49,11 @@ module EventSource
         # @param [Object] subscriber_klass Subscriber class
         # @return [Queue] Queue instance
         def subscribe(subscriber_klass, _options)
+          subscription_key = [app_name, formatted_exchange_name].join(delimiter)
           subscriber_suffix = subscriber_klass.name.downcase.gsub('::', '_')
-          unique_key = [app_name, formatted_exchange_name].join(delimiter) + "_#{subscriber_suffix}"
-          logger.debug "FaradayQueueProxy#register_subscription Subscriber Class #{subscriber_klass}"
-          logger.debug "FaradayQueueProxy#register_subscription Unique_key #{unique_key}"
+          unique_key = subscription_key + "_#{subscriber_suffix}"
+          logger.info "FaradayQueueProxy#register_subscription Subscriber Class #{subscriber_klass}"
+          logger.info "FaradayQueueProxy#register_subscription Unique_key #{unique_key}"
           @subject.register_action(subscriber_klass, unique_key)
         end
 
--- spec/event_source/rails_application_spec.rb
+++ spec/event_source/rails_application_spec.rb
@@ -2,12 +2,13 @@ require "spec_helper"
 require "parallel_tests"
 require "parallel_tests/rspec/runner"
 
-RSpec.describe EventSource, "railtie specs" do
+RSpec.describe EventSource, "rails specs" do
   it "runs the rails tests in the rails application context" do
     ParallelTests.with_pid_file do
       specs_run_result = ParallelTests::RSpec::Runner.run_tests(
         [
-          "spec/rails_app/spec/railtie_spec.rb"
+          "spec/rails_app/spec/railtie_spec.rb",
+          "spec/rails_app/spec/http_service_integration_spec.rb"
         ],
         1,
         1,
